5e029344d677a7acca29005a2b6470fb6c4a1e33
itachiuchiha10nd <itachiuchiha10nd@gmail.com>
2026-06-17 17:37:23 +0530

    Create wishes1.js

---
+++ wishes1.js
@@ -0,0 +1,27 @@
+const wishes = [];
+
+function addWish(w){
+wishes.push(w);
+}
+
+/* ================= BIRTHDAY WISHES 2026 ================= */
+
+addWish({
+title:"Hi babe…",
+msg:`It feels just like yesterday we met each other in FAMILY haha.
+Yet, from the first moment I saw you, I knew I wanted to be your friend haha ><
+
+Just like how DSK described you, you’re really good at playing games. I don’t know what tricks you used, but I secretly wished we were allies lol.
+
+As time goes by, I realize that you’re a normal person too. You get scared, you get mad, and you even cry when things get rough. But what makes you different from the rest..from me, is that you are better at expressing them. You are braver than you think, babe :)
+
+I’m so proud to be allowed to be here... To enjoy these moments with you, to watch us grow into wonderful people while still having fun along the way ^^
+
+Thank you so much for being the Zinc we all know. I love you, and I want you to know that ^^
+
+Happy birthday, sister 🥳🎂
+May this year be amaZinc, just like you :3
+
+- REN`,
+  color:"#d4edda"
+  });
